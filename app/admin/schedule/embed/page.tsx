9d38efc5d13a9e66d49e1c39b43e4a477747cd8a
Humza Muhammad <humzam241@outlook.com>
2025-08-11 13:57:46 -0400

    Auto-deploy: 2025-08-11 13:57:46

--- app/admin/schedule/embed/page.tsx
+++ app/admin/schedule/embed/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect } from "react";
-import AdminSchedulePage from "../../page";
+import AdminSchedulePage from "../page";
 
 export default function AdminScheduleEmbed() {
   useEffect(() => {
